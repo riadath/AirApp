a26a458c0e39dd278d00c083b271b938bba0121b
riadath <akib152012@gmail.com>
2022-03-26 22:54:57 +0600

    First Commit

--- src/main/java/main/airapp/Application.java
+++ src/main/java/main/airapp/Application.java
@@ -9,9 +9,10 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello-!!!!");
         stage.setScene(scene);
         stage.show();
     }
