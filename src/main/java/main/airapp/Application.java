01df39efa7fd66deb7b399622157982409dcaf44
KrabbyKrabby <chowdhuryittehad@gmail.com>
2022-03-26 22:51:12 +0600

    Commit by Ittehad

--- src/main/java/main/airapp/Application.java
+++ src/main/java/main/airapp/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello!!!!!");
         stage.setScene(scene);
         stage.show();
     }
