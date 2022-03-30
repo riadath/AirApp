e191d5728f2c57460058a0590909618a41c56201
KrabbyKrabby <chowdhuryittehad@gmail.com>
2022-03-30 22:46:48 +0600

    Added Flight Schedule

--- src/main/java/main/airapp/UserMenuPageController.java
+++ src/main/java/main/airapp/UserMenuPageController.java
@@ -11,5 +11,12 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class UserMenuPageController extends Controller{
-
+    @FXML
+    public void switchToFlightSchedule(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("flight-schedule-page.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
