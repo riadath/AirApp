d230fdde6fec18bc61c1f9ac52903ec9ca317b70
KrabbyKrabby <chowdhuryittehad@gmail.com>
2022-03-30 22:54:14 +0600

    Added Flight Schedule

--- src/main/java/main/airapp/FlightScheduleController.java
+++ src/main/java/main/airapp/FlightScheduleController.java
@@ -3,19 +3,26 @@ package main.airapp;
 import datamodel.Flight;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class FlightScheduleController implements Initializable {
+public class FlightScheduleController extends Controller implements Initializable{
 
     @FXML
     private TableView<Flight> FlightScheduler;
@@ -61,7 +68,13 @@ public class FlightScheduleController implements Initializable {
         DepartureTime.setCellValueFactory( new PropertyValueFactory<>("DepartureTime"));
 
         FlightScheduler.setItems(( list ));
+    }
 
-
+    public void switchToUserMenu(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("user-menu-page.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
