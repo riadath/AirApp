8b8b40225ac58f07c66ab442bf0a69346be9420e
master-da <mdnokib2000@gmail.com>
2022-04-04 03:10:47 +0600

    Flight and Airplanes from DB. needs some work.

--- src/main/java/main/airapp/Application.java
+++ src/main/java/main/airapp/Application.java
@@ -11,6 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("admin-panel-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello-!!!!");
         stage.setScene(scene);
