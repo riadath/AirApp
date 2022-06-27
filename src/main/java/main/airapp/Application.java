a3bc13f39605669a9c6faabab875c6ebeb9bdc30
riadath <akib152012@gmail.com>
2022-06-27 21:03:09 +0600

    Made minor UI changes on login screen

--- src/main/java/main/airapp/Application.java
+++ src/main/java/main/airapp/Application.java
@@ -10,8 +10,8 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-//        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("admin-panel-page.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-page.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("admin-panel-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("AirApp");
         stage.setScene(scene);
